a5d01af245616e1a6ebb875d1aa4fb48530af229
jade decordova <lacroixjaded@gmail.com>
2020-07-30 07:42:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,10 @@
-def everse_each_word(str)
+def reverse_each_word(str)
   array = str.split
   array.each {|word| word.reverse}
+end
+
+def reverse_each_word(str)
+  array = str.split
+  array.collect{ |word| word.reverse}
+
 end
