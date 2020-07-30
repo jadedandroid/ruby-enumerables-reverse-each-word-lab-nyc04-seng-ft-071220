0589aa700487f7f935efa4277d4f68da24418b0f
jade decordova <lacroixjaded@gmail.com>
2020-07-30 07:59:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require "pry"
 
 def reverse_each_word(str)
   array = str.split()
-  array.each {|word| word.reverse.to_str.concat}
+  reverse = array.each {|word| word.reverse.to_str.concat}
 end
 
 def reverse_each_word(str)
